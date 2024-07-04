b536eb4d95e99d4f587535b2703f273fe069afa7
Pavez7274 <tuviejale@gmail.com>
2024-07-04 18:31:30 -0300

    feat: Add core/token module for token management

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export * from "./core/base.competence";
 export * from "./core/base.transpiler";
 export * from "./core/registry";
 export * from "./core/schema";
+export * from "./core/token";
 export * from "./core/lexer";
 export * from "./core/node";
