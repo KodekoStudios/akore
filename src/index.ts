c807f6366318c2202cfa52047cb80187ba6c752e
Pavez7274 <tuviejale@gmail.com>
2024-10-03 01:36:42 -0300

    I forgor :3 <img src="https://i.pinimg.com/736x/c1/9c/d1/c19cd1f111ff7195ecf7a79f1530f01e.jpg" alt="i forgor" />

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./common/";
 export * from "./core/base.competence";
 export * from "./core/base.transpiler";
 export * from "./core/registry";
+export * from "./core/reader";
 export * from "./core/schema";
 export * from "./core/token";
 export * from "./core/lexer";
